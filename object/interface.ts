c8b9a10e90cc7058d19e3d848297fa0331e61ea5
MIKIO ODA <oda.mikio@gmail.com>
2020-08-15 09:28:26 +0900

    ...

---
+++ object/interface.ts
@@ -0,0 +1,20 @@
+/*
+interface ITest {
+    S1: string,
+    N1: number,
+    O: {
+        S2: string,
+        N2: number,
+    }
+}
+*/
+
+
+const O: {S1: string,N1: number,O: {S2: string,N2: number}} = {
+    S1: "STRING1",
+    N1: 1,
+    O: {
+        S2: "STRING2",
+        N2: 2,
+    }
+}
