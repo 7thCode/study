2d4de2c3e8b4bcff3798563136f82e92cadaeb3e
MIKIO ODA <oda.mikio@gmail.com>
2020-08-22 10:52:35 +0900

    LivingThing

--- src/typescript/oda.ts
+++ src/typescript/oda.ts
@@ -1,5 +1,61 @@
 namespace Oda {
 
+class LivingThing {
+    bless_count: number;
+    moved: boolean;
+    X: number;
+    Y: number;
+
+    constructor(bless: number) {
+        this.X = 0;
+        this.Y = 0;
+        this.bless_count = bless;
+        this.moved = true;
+    }
+
+    walk() {
+     this.X++;
+    } 
+}
+
+class Human extends LivingThing {
+    think: boolean;
+
+    constructor() {
+        super(60);
+        this.think = true;
+    }
+}
+
+class Person extends Human {
+    name : string;
+
+    constructor() {
+        super();
+        this.name = "";
+    }
+}
+
+class OfficeWoker extends Person {
+    company: string;
+
+    constructor(company: string) {
+        super();
+        this.company = company;
+    }
+}
+
+const oda = new OfficeWoker("ABC");
+
+if (oda.moved) {
+   console.info(oda.X);
+   oda.walk();
+   console.info(oda.X);
+}
+
+
+
+
 // クラスの基本
 
 /*
